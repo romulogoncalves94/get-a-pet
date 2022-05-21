7dbdf2baa6c9c6662aedb99249f5a6d5aa6857fe
romulogoncalves94 <romulogoncalves94@hotmail.com>
2022-05-21 14:56:15 -0300

    feat: criado verificação de usuário por token e resgatando usuário pelo id

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -1,6 +1,8 @@
 const User = require('../models/User')
 const bcrypt = require('bcrypt')
 const createUserToken = require('../helpers/create-user-token')
+const getToken = require('../helpers/get-token')
+const jwt = require('jsonwebtoken')
 
 module.exports = class UserController {
 
@@ -103,6 +105,50 @@ module.exports = class UserController {
         }
 
         await createUserToken(user, req, res)
+
+    }
+
+    static async checkUser(req, res){
+
+        let currentUser
+
+        console.log(req.headers.authorization)
+
+        if(req.headers.authorization){
+            const token = getToken(req)
+            const decoded = jwt.verify(token, 'nossosecret')
+            
+            currentUser = await User.findById(decoded.id)
+
+            currentUser.password = undefined
+
+        } else{
+            currentUser = null
+        }
+
+        res.status(200).send(currentUser)
+
+    }
+
+    static async getUserById(req, res){
+
+        const id = req.params.id
+
+        const user = await User.findById(id).select('-password')
+
+        if(!user){
+            res.status(422).json({ message: 'Usuário não encontrado!' })
+            return
+        }
+
+        res.status(200).json({ user })
+
+    }
+
+    static async editUser(req, res){
+
+        res.status(200).json({ message: 'Usuário atualizado com sucesso!'})
+        return
 
     }
 }
