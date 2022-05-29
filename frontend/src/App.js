f8d89b1a403b20184f5a7308278cfafbf0cef09a
romulogoncalves94 <romulogoncalves94@hotmail.com>
2022-05-29 18:54:34 -0300

    feat: config React, criação de Navbar e Footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,29 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+/* Pages */
+import Login from './components/pages/Auth/Login'
+import Register from './components/pages/Auth/Register'
+import Home from './components/pages/Home'
+
+/* Components */
+import Navbar from './components/layout/Navbar'
+import Footer from './components/layout/Footer'
+import Container from './components/layout/Container'
 
 
 function App() {
   return (
-    <div className="App">
-      <h1>Get A Pet</h1>
-    </div>
+    <Router>
+      <Navbar />
+        <Container>
+        <Routes>
+          <Route path='/login' element={<Login />}/>
+          <Route path='/register' element={<Register />}/>
+          <Route path='/' element={<Home />}/>
+        </Routes>
+        </Container>
+      <Footer />
+    </Router>
   );
 }
 
